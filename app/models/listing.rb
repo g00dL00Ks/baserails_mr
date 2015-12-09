7dfa1c7ef1455fef020ddaa3b4de990f306a7dde
Mitzi Reaugh <mitzi@goodlooks.me>
2015-12-09 14:12:22 -0800

    removed dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,14 @@
 class Listing < ActiveRecord::Base
-  if Rails.env.development?  
+  #removed as dropbox isn't working
+  #if Rails.env.development?  
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg"
-  else
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml") ,
-                    :path => ":style/:id_:filename"
-  end
+  
+  #else
+   # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg",
+   #                 :storage => :dropbox,
+   #                 :dropbox_credentials => Rails.root.join("config/dropbox.yml") ,
+   #                 :path => ":style/:id_:filename"
+  #end
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, :description, :price, presence: true
